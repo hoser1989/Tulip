fea35ded8cdac23dfaab53d0833d9f66422f9b4d
kosja <jacekkos89@gmal.com>
2025-03-18 09:51:34 +0100

    Date inpur parameters defined, handing empty records when loading the data from Production Orders table

--- update_traveller_and_bom.py
+++ update_traveller_and_bom.py
@@ -3,156 +3,13 @@ import pandas as pd
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+from datetime import datetime, timedelta
 
-def sendToTulip(df, table_id):
-    url = f'https://hiab-test.tulip.co/api/v3/tables/{table_id}/records'
-    user = 'apikey.2_YbECmsfBSjhGwYf3T'
-    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
-
-    with requests.Session() as session:
-        session.auth = HTTPBasicAuth(user, pwd)
-
-        # Iterate and send requests
-        for index, row in df.iterrows():
-            payload = row.to_dict()  # Convert row to dictionary (JSON-like)
-
-            try:
-                print(payload)
-                response = session.post(url, json=payload)
-                response.raise_for_status()  # Raise exception for bad responses
-
-                #print(f"Row {index} sent successfully: {response.json()}")
-
-            except requests.exceptions.RequestException as e:
-                print(f"Error sending row {index}: {e}")
-
-def updateTulipSyncRecord(record_id, fieldId):
-    url_base = 'https://hiab-test.tulip.co/api/v3/tables/F8msta7LuWpSHqXPz/records'
-    user = 'apikey.2_YbECmsfBSjhGwYf3T'
-    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
-
-    with requests.Session() as session:
-        session.auth = HTTPBasicAuth(user, pwd)
-
-        url = f"{url_base}/{record_id}"  # Tworzenie URL dla PUT
-
-        payload = {
-                f"{fieldId}" : True
-            }
-
-        try:
-            print(f"Updating record {record_id}: {payload}")
-            response = session.put(url, json=payload)
-            response.raise_for_status()  # Sprawdzenie błędów
-
-            print(f"Record {record_id} updated successfully: {response.json()}")
-
-        except requests.exceptions.RequestException as e:
-            print(f"Error updating record {record_id}: {e}")
-
-def createTulipSyncRecord(record_id):
-    url = 'https://hiab-test.tulip.co/api/v3/tables/F8msta7LuWpSHqXPz/records'
-    user = 'apikey.2_YbECmsfBSjhGwYf3T'
-    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
-
-    # payload
-    payload = {
-        "id": record_id,
-        "jrbjn_traveller": False ,
-        "uazuq_bom": False,
-    }
-
-    with requests.Session() as session:
-        session.auth = HTTPBasicAuth(user, pwd)
-
-        try:
-            print(f"Sending data: {payload}")
-            response = session.post(url, json=payload)
-            response.raise_for_status()  # Check error
-
-            print(f"Record {record_id} sent successfully: {response.json()}")
-
-        except requests.exceptions.RequestException as e:
-            print(f"Error sending record {record_id}: {e}")
-
-
-def checkIfTulipOrderExists(order_number):
-    user = 'apikey.2_YbECmsfBSjhGwYf3T'
-    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
-    url = 'https://hiab-test.tulip.co/api/v3/tables/Jii944sA7s3kS5pu8_DEFAULT/records?filters=[{"field":"id","arg":"' + order_number + '","functionType":"equal"}]&limit=1'
-
-    headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
-    response = requests.get(url, auth=(user, pwd), headers=headers)
-
-    if response.status_code != 200:
-        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
-        exit()
-
-    data = response.json()
-    return data
-
-def checkTulipIfSynced(order_number):
-    user = 'apikey.2_YbECmsfBSjhGwYf3T'
-    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
-    url = 'https://hiab-test.tulip.co/api/v3/tables/F8msta7LuWpSHqXPz/records?filters=[{"field":"id","arg":"' + order_number + '","functionType":"equal"}]&limit=1'
-
-    headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
-    response = requests.get(url, auth=(user, pwd), headers=headers)
-
-    if response.status_code != 200:
-        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
-        exit()
-
-    data = response.json()
-
-    if not isinstance(data, list):  # Check, if JSON is a list
-        print("Błąd: Oczekiwano listy, ale otrzymano:", type(data))
-        return None
-
-    df = pd.DataFrame(data)
-    return df
-
-def getTulipProductionOrders(datetime_from, datetime_to, all = False):
-    user = 'apikey.2_YbECmsfBSjhGwYf3T'
-    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
-
-    filters = [
-        {"field": "_createdAt", "functionType": "greaterThanOrEqual", "arg": datetime_from},
-        {"field": "_createdAt", "functionType": "lessThan", "arg": datetime_to}
-    ]
-
-    filters_str = json.dumps(filters)  # Convert to proper JSON format
-    if all == False:
-        url = f'https://hiab-test.tulip.co/api/v3/tables/Jii944sA7s3kS5pu8_DEFAULT/records?filters={filters_str}'
-    else:
-        url = f'https://hiab-test.tulip.co/api/v3/tables/Jii944sA7s3kS5pu8_DEFAULT/records'
-
-    headers = {"Content-Type": "application/json", "Accept": "application/json"}
-
-    response = requests.get(url, auth=(user, pwd), headers=headers)
-
-    if response.status_code != 200:
-        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
-        exit()
-
-    data = response.json()
-
-    if not isinstance(data, list):  # Check if JSON is a list
-        print("Błąd: Oczekiwano listy, ale otrzymano:", type(data))
-        return None
-
-    df = pd.DataFrame(data)  # Create DataFrame
-
-    production_order = df[['id']]
-
-    return production_order  # Dataframe result
-
+# LN
 def productionSpecificationSummary(order_number):
     # Connect to DB
     or_dns = oracledb.makedsn('134.37.15.96', 1522, service_name='erpln6c')
-    con = oracledb.connect(user="ecq", password="ecq105", dsn=or_dns)
+    con = oracledb.connect(user="tulip215db", password="tulip215f0rtulip215", dsn=or_dns)
     cursor = con.cursor()
 
     # SQL query
@@ -183,7 +40,7 @@ def productionSpecificationSummary(order_number):
     'PRT06','PRT07','PRT08','PRT09','PRT10','PRT11')
     where ticst001.t$pdno = :production_order
     order by tcibd001.t$cdf_popt
-    FETCH FIRST 10 ROWS ONLY"""
+    """
 
     # Execute query
     cursor.execute(sql, {'production_order': order_number})
@@ -211,7 +68,7 @@ def productionSpecificationSummary(order_number):
 def billOfMaterial(order_number):
     # Connect to DB
     or_dns = oracledb.makedsn('134.37.15.96', 1522, service_name='erpln6c')
-    con = oracledb.connect(user="ecq", password="ecq105", dsn=or_dns)
+    con = oracledb.connect(user="tulip215db", password="tulip215f0rtulip215", dsn=or_dns)
     cursor = con.cursor()
 
     # SQL query
@@ -261,32 +118,268 @@ def billOfMaterial(order_number):
     cursor.close()
     con.close()
     return df
+#---
+
+def sendToTulip(df, table_id):
+    url = f'https://hiab.tulip.co/api/v3/tables/{table_id}/records'
+    user = 'apikey.2_YbECmsfBSjhGwYf3T'
+    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
+
+    with requests.Session() as session:
+        session.auth = HTTPBasicAuth(user, pwd)
+
+        # Iterate and send requests
+        for index, row in df.iterrows():
+            payload = row.to_dict()  # Convert row to dictionary (JSON-like)
+
+            try:
+                print(payload)
+                response = session.post(url, json=payload)
+                response.raise_for_status()  # Raise exception for bad responses
+
+                #print(f"Row {index} sent successfully: {response.json()}")
+
+            except requests.exceptions.RequestException as e:
+                print(f"Error sending row {index}: {e}")
+
+def updateTulipSyncRecord(record_id, fieldId, fieldValue):
+    url_base = 'https://hiab.tulip.co/api/v3/tables/F8msta7LuWpSHqXPz/records'
+    user = 'apikey.2_YbECmsfBSjhGwYf3T'
+    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
+
+    with requests.Session() as session:
+        session.auth = HTTPBasicAuth(user, pwd)
+
+        url = f"{url_base}/{record_id}"  # Create URL for PUT
+
+        payload = {
+                f"{fieldId}" : fieldValue
+            }
+
+        try:
+            print(f"Updating record {record_id}: {payload}")
+            response = session.put(url, json=payload)
+            response.raise_for_status()  # Sprawdzenie błędów
+
+            print(f"Record {record_id} updated successfully: {response.json()}")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error updating record {record_id}: {e}")
+
+def createTulipSyncRecord(record_id):
+    url = 'https://hiab.tulip.co/api/v3/tables/F8msta7LuWpSHqXPz/records'
+    user = 'apikey.2_YbECmsfBSjhGwYf3T'
+    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
+
+    # payload
+    payload = {
+        "id": record_id,
+        "jrbjn_traveller": False ,
+        "uazuq_bom": False,
+    }
+
+    with requests.Session() as session:
+        session.auth = HTTPBasicAuth(user, pwd)
+
+        try:
+            print(f"Sending data: {payload}")
+            response = session.post(url, json=payload)
+            response.raise_for_status()  # Check error
+
+            print(f"Record {record_id} sent successfully: {response.json()}")
+
+        except requests.exceptions.RequestException as e:
+            print(f"Error sending record {record_id}: {e}")
+
+
+def checkIfTulipOrderExists(order_number):
+    user = 'apikey.2_YbECmsfBSjhGwYf3T'
+    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
+    url = 'https://hiab.tulip.co/api/v3/tables/Jii944sA7s3kS5pu8_DEFAULT/records?filters=[{"field":"id","arg":"' + order_number + '","functionType":"equal"}]&limit=1'
+
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    response = requests.get(url, auth=(user, pwd), headers=headers)
+
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+        exit()
+
+    data = response.json()
+    return data
+
+def checkTulipIfSynced(order_number):
+    user = 'apikey.2_YbECmsfBSjhGwYf3T'
+    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
+    url = 'https://hiab.tulip.co/api/v3/tables/F8msta7LuWpSHqXPz/records?filters=[{"field":"id","arg":"' + order_number + '","functionType":"equal"}]&limit=1'
+
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    response = requests.get(url, auth=(user, pwd), headers=headers)
+
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+        exit()
+
+    data = response.json()
+
+    if not isinstance(data, list):  # Check, if JSON is a list
+        print("Błąd: Oczekiwano listy, ale otrzymano:", type(data))
+        return None
+
+    df = pd.DataFrame(data)
+    return df
+
+def getTulipProductionOrders(datetime_from, datetime_to, all = False):
+    user = 'apikey.2_YbECmsfBSjhGwYf3T'
+    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
+
+    filters = [
+        {"field": "_createdAt", "functionType": "greaterThanOrEqual", "arg": datetime_from},
+        {"field": "_createdAt", "functionType": "lessThan", "arg": datetime_to},
+        # {"field": "levog_status", "functionType": "isIn", "arg": ['Active', 'Released']}
+    ]
+
+    filters_str = json.dumps(filters)  # Convert to proper JSON format
+    if all == False:
+        url = f'https://hiab.tulip.co/api/v3/tables/Jii944sA7s3kS5pu8_DEFAULT/records?filters={filters_str}'
+    else:
+        url = f'https://hiab.tulip.co/api/v3/tables/Jii944sA7s3kS5pu8_DEFAULT/records'
+
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    response = requests.get(url, auth=(user, pwd), headers=headers)
+
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+        exit()
+
+    data = response.json()
+
+    if not isinstance(data, list):  # Check if JSON is a list
+        print("Error: List was expected, but received:", type(data))
+        return None
+
+    # If no records were found, return an empty DataFrame
+    if not data:
+        return pd.DataFrame()  # Return empty DataFrame if no data
+
+    df = pd.DataFrame(data)  # Create DataFrame
+
+    production_order = df[['id']]
+
+    return production_order  # Dataframe result
+
+def getActiveTulipProductionOrders():
+    user = 'apikey.2_YbECmsfBSjhGwYf3T'
+    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
+
+    filters = [
+        {"field": "levog_status", "functionType": "isIn", "arg": ['Active', 'Released']}
+    ]
+
+    filters_str = json.dumps(filters)  # Convert to proper JSON format
+
+    url = f'https://hiab.tulip.co/api/v3/tables/Jii944sA7s3kS5pu8_DEFAULT/records?filters={filters_str}&limit=100&offset=200'
+
+
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    response = requests.get(url, auth=(user, pwd), headers=headers)
+
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+        exit()
+
+    data = response.json()
+
+    if not isinstance(data, list):  # Check if JSON is a list
+        print("Error: List was expected, but received:", type(data))
+        return None
+
+        # If no records were found, return an empty DataFrame
+    if not data:
+        return pd.DataFrame()  # Return empty DataFrame if no data
+
+    df = pd.DataFrame(data)  # Create DataFrame
+
+    production_order = df[['id']]
+
+    return production_order  # Dataframe result
+
+
+def getActiveTulipProductionOrdersALL():
+    user = 'apikey.2_YbECmsfBSjhGwYf3T'
+    pwd = 'GO01NhVXEikyXQ-uksiQ4v6nPplEtoAW-sVklVtUAfs'
+
+    filters = [
+        {"field": "levog_status", "functionType": "isIn", "arg": ['Active', 'Released']}
+    ]
+
+    filters_str = json.dumps(filters)  # Convert to proper JSON format
+
+    base_url = 'https://hiab.tulip.co/api/v3/tables/Jii944sA7s3kS5pu8_DEFAULT/records'
+    headers = {"Content-Type": "application/json", "Accept": "application/json"}
+
+    all_records = []
+    offset = 0
+    limit = 100  # Max Limit
+
+    while True:
+        url = f'{base_url}?filters={filters_str}&limit={limit}&offset={offset}'
+        response = requests.get(url, auth=(user, pwd), headers=headers)
+
+        if response.status_code != 200:
+            print('Status:', response.status_code, 'Headers:', response.headers, 'Error Response:', response.json())
+            break
+
+        data = response.json()
+
+        if not isinstance(data, list) or len(data) == 0:  # Check if list is empty
+            break
+
+        all_records.extend(data)
+        offset += limit  # We shift the offset by the limit to fetch the next records
+
+    df = pd.DataFrame(all_records)  # Create DataFrame
+
+    return df[['id']]  # Retrurn only "id"
 
 
 #Execute
-result = getTulipProductionOrders('2024-11-25T00:00:00Z','2024-11-26T00:00:00Z')
-for index,row in result.iterrows():
-    sync_status = checkTulipIfSynced(row.id)
-    if sync_status.empty:
-        createTulipSyncRecord(row.id)
-        # update production specification summary
-        pss = productionSpecificationSummary(row.id)
-        sendToTulip(pss, 'JB6jTG755K3BFFAyS')
-        updateTulipSyncRecord(row.id, 'jrbjn_traveller')
-        # update bom
-        bom = billOfMaterial(row.id)
-        sendToTulip(bom, 'K6y2f8AiFpK8SbscT')
-        updateTulipSyncRecord(row.id, 'uazuq_bom')
-    else:
-        if not sync_status['jrbjn_traveller'].values:
+beginning_of_the_day = datetime.combine(datetime.today(), datetime.min.time())
+beginning_of_the_day_str = beginning_of_the_day.strftime('%Y-%m-%dT%H:%M:%SZ')
+end_of_the_day = beginning_of_the_day + timedelta(days=1)
+end_of_the_day_str = end_of_the_day.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+result = getTulipProductionOrders(beginning_of_the_day_str, end_of_the_day_str)
+
+if not result.empty:
+    for index,row in result.iterrows():
+        sync_status = checkTulipIfSynced(row.id)
+        if sync_status.empty:
+            createTulipSyncRecord(row.id)
             # update production specification summary
             pss = productionSpecificationSummary(row.id)
             sendToTulip(pss, 'JB6jTG755K3BFFAyS')
-            updateTulipSyncRecord(row.id, 'jrbjn_traveller')
-        if not sync_status['uazuq_bom'].values:
+            updateTulipSyncRecord(row.id, 'jrbjn_traveller', True)
             # update bom
             bom = billOfMaterial(row.id)
             sendToTulip(bom, 'K6y2f8AiFpK8SbscT')
-            updateTulipSyncRecord(row.id, 'uazuq_bom')
+            updateTulipSyncRecord(row.id, 'uazuq_bom', True)
+        else:
+            if not sync_status['jrbjn_traveller'].values:
+                # update production specification summary
+                pss = productionSpecificationSummary(row.id)
+                sendToTulip(pss, 'JB6jTG755K3BFFAyS')
+                updateTulipSyncRecord(row.id, 'jrbjn_traveller', True)
+            if not sync_status['uazuq_bom'].values:
+                # update bom
+                bom = billOfMaterial(row.id)
+                sendToTulip(bom, 'K6y2f8AiFpK8SbscT')
+                updateTulipSyncRecord(row.id, 'uazuq_bom', True)
+else:
+    print('No data returned.')
+
 
 
