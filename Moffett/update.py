0fab75810d065803ed2840190926c7e8d67109e9
kosja <jacekkos89@gmal.com>
2025-04-29 22:24:13 +0200

    hastag removed

--- Moffett/update.py
+++ Moffett/update.py
@@ -11,8 +11,8 @@ beginning_of_the_day_str = beginning_of_the_day.strftime('%Y-%m-%dT%H:%M:%SZ')
 end_of_the_day = beginning_of_the_day + timedelta(days=1)
 end_of_the_day_str = end_of_the_day.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-#result = api_get.getProductionOrders('Jii944sA7s3kS5pu8_DEFAULT', beginning_of_the_day_str, end_of_the_day_str)
-#
+result = api_get.getProductionOrders('Jii944sA7s3kS5pu8_DEFAULT', beginning_of_the_day_str, end_of_the_day_str)
+
 production_orders_table_id = 'Jii944sA7s3kS5pu8_DEFAULT'
 sync_table_id = 'F8msta7LuWpSHqXPz'
 oms_conf_table_id = 'HpCsSgXsoWxPuQTit'
@@ -20,7 +20,7 @@ oms_sales_order_data_table_id = 'xkHFpjXYMbE2heyn5'
 pss_table_id = 'JB6jTG755K3BFFAyS'
 bom_table_id = 'K6y2f8AiFpK8SbscT'
 
-result = api_get.getActiveProductionOrdersALL('Jii944sA7s3kS5pu8_DEFAULT')
+# result = api_get.getActiveProductionOrdersALL('Jii944sA7s3kS5pu8_DEFAULT')
 
 if not result.empty:
     for index,row in result.iterrows():
